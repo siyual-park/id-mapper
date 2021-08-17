e7e418c7d0441f794ae07dc1cefc4c31c67eaacd
ara.bak <siyual.bak@gmail.com>
2021-08-17 09:26:34 +0900

    refactoring

--- id_mapper/dataset/coco.py
+++ id_mapper/dataset/coco.py
@@ -7,43 +7,52 @@ from urllib import request
 from torch.utils.data import Dataset
 
 
+def _load(remote: str, local: Path, cache: Path) -> None:
+    if not os.path.exists(local):
+        if not os.path.exists(cache):
+            print(f'Download dataset from {remote} to {cache}')
+            _download(remote, cache)
+
+        print(f'Unzip dataset from {cache} to {local}')
+        _unzip(cache, local)
+
+        print(f'Remove cached dataset from {cache}')
+        os.remove(cache)
+
+
+def _download(remote: str, local: Path) -> None:
+    request.urlretrieve(remote, local)
+
+
+def _unzip(origin: Path, to: Path) -> None:
+    with zipfile.ZipFile(origin, 'r') as zip_ref:
+        zip_ref.extractall(to)
+
+
 class COCO(Dataset):
     def __init__(
             self,
-            remote: Optional[Path or str],
-            local: Path or str
+            remote: Optional[str],
+            local: Path or str,
+            annotation_remote: Optional[str],
+            annotation_local: Path or str
     ):
         self.__remote = remote
+        self.__annotation_remote = annotation_remote
 
         self.__local = Path(local)
+        self.__annotation_local = Path(annotation_local)
+
         self.__local_zip = Path(local).with_suffix('.zip')
+        self.__annotation_local_zip = Path(annotation_local).with_suffix('.zip')
 
         self.__local.parent.mkdir(parents=True, exist_ok=True)
+        self.__annotation_local.parent.mkdir(parents=True, exist_ok=True)
         self.__local_zip.parent.mkdir(parents=True, exist_ok=True)
+        self.__annotation_local_zip.parent.mkdir(parents=True, exist_ok=True)
 
-        if not os.path.exists(self.__local):
-            if not os.path.exists(self.__local_zip):
-                print(f'Download dataset from {self.__remote} to {self.__local_zip}')
-                self.__download()
-
-            print(f'Unzip dataset from {self.__local_zip} to {self.__local}')
-            self.__unzip()
-
-            print(f'Remove cached dataset from {self.__local_zip}')
-            self.__remove_zip()
-
-    def __download(self) -> None:
-        if self.__remote is None:
-            raise Exception()
-
-        request.urlretrieve(self.__remote, self.__local_zip)
-
-    def __unzip(self) -> None:
-        with zipfile.ZipFile(self.__local_zip, 'r') as zip_ref:
-            zip_ref.extractall(self.__local)
-
-    def __remove_zip(self) -> None:
-        os.remove(self.__local_zip)
+        _load(self.__remote, self.__local, self.__local_zip)
+        _load(self.__annotation_remote, self.__local, self.__local_zip)
 
 
 if __name__ == '__main__':
