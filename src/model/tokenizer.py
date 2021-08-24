a7bfe75f48c8aa24c3e8ce10313578c6edc47962
ara.bak <siyual.bak@gmail.com>
2021-08-25 00:03:32 +0900

    model compression

--- src/model/tokenizer.py
+++ src/model/tokenizer.py
@@ -13,6 +13,9 @@ class ResBlock(nn.Module):
             channels: int,
             kernel_size: size_2_t = 3,
             groups: int = 1,
+            pooling_kernel_size: size_2_t = 2,
+            pooling_stride: size_2_t = 2,
+            pooling_dilation: size_2_t = 1,
             expansion: float = 0.5,
             dropout_prob: float = 0.0
     ):
@@ -26,39 +29,6 @@ class ResBlock(nn.Module):
             expansion=expansion,
             dropout_prob=dropout_prob,
         )
-
-        self.shortcut = Shortcut()
-
-    def forward(self, x):
-        x_out = self.conv(x)
-
-        return self.shortcut(x, x_out)
-
-
-class FeatureCompression(nn.Module):
-    def __init__(
-            self,
-            channels: int,
-            kernel_size: size_2_t = 3,
-            groups: int = 1,
-            pooling_kernel_size: size_2_t = 2,
-            pooling_stride: size_2_t = 2,
-            pooling_dilation: size_2_t = 1,
-            expansion: float = 0.5,
-            dropout_prob: float = 0.0
-    ):
-        super().__init__()
-
-        self.res_block = ResBlock(
-            channels,
-            kernel_size,
-            groups,
-            expansion,
-            dropout_prob
-        )
-        self.attention = CBAM(
-            gate_channels=channels
-        )
         self.pooling = nn.MaxPool2d(
             kernel_size=pooling_kernel_size,
             stride=pooling_stride,
@@ -66,9 +36,11 @@ class FeatureCompression(nn.Module):
             padding=autopad(pooling_kernel_size)
         )
 
+        self.shortcut = Shortcut()
+
     def forward(self, x):
-        x_out = self.res_block(x)
-        x_out = self.attention(x_out)
+        x_out = self.conv(x)
+        x_out = self.shortcut(x, x_out)
         x_out = self.pooling(x_out)
 
         return x_out
@@ -102,8 +74,8 @@ class Tokenizer(nn.Module):
         pooling_kernel_size = 3
         pooling_dilation = 2
 
-        self.compression = nn.Sequential(*[
-            FeatureCompression(
+        res_blocks = [
+            ResBlock(
                 channels=channels,
                 dropout_prob=dropout_prob,
                 pooling_kernel_size=pooling_kernel_size,
@@ -111,7 +83,14 @@ class Tokenizer(nn.Module):
                 pooling_dilation=pooling_dilation
             )
             for _ in range(deep)
-        ])
+        ]
+        self.compression = nn.Sequential(
+            *res_blocks,
+            CBAM(
+                gate_channels=channels,
+                dropout_prob=dropout_prob
+            )
+        )
 
         if isinstance(image_size, int):
             image_size = (image_size, image_size)
@@ -119,17 +98,7 @@ class Tokenizer(nn.Module):
         sample = self.compression(torch.zeros((1, channels, image_size[0], image_size[1])))
         batch, channels, w, h = sample.size()
 
-        self.feature_compression = nn.Sequential(
-            nn.Linear(w * h, int(w * h * 0.5)),
-            nn.ReLU(),
-            nn.Linear(int(w * h * 0.5), 1),
-        )
-
-        self.feature_expand = nn.Sequential(
-            nn.Linear(channels, channels),
-            nn.ReLU(),
-            nn.Linear(channels, token_size),
-        )
+        self.feature_compression = nn.Linear(w * h, 1)
 
     def forward(self, x):
         # x (batch, channel, w, h)
@@ -141,9 +110,7 @@ class Tokenizer(nn.Module):
         x_out = x_out.view(batch * channel, -1)
 
         x_out = self.feature_compression(x_out)
-
         x_out = x_out.view(batch, channel)
-        x_out = self.feature_expand(x_out)
 
         return x_out
 
